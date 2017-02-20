563ff661b6af57b5506ed44632a4891c0e7fa3b9
MaximSkavysh <maximgen666@gmail.com>
2017-02-20 15:14:51 +0300

    News

---
+++ Store.Domain/Entities/News.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Store.Domain.Entities
+{
+    public class News
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public byte[] ImageData { get; set; }
+        [HiddenInput(DisplayValue = false)]
+        public string ImageType { get; set; }
+    }
+}
